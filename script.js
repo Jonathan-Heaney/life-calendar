65bf03e334b111e6ecde94bd5e6eebcb0edb3883
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-09-09 17:16:05 -0500

    Refactor for loops in JS, only one loop now

--- script.js
+++ script.js
@@ -16,62 +16,50 @@ const lateAdulthoodContainer = document.querySelector(
 );
 
 function childhoodBubbles() {
-  for (let i = 0; i < 52; i++) {
-    for (let j = 0; j < 12; j++) {
-      const bubble = document.createElement('div');
-      bubble.classList.add('childhood', 'bubble');
-      childhoodContainer.append(bubble);
-    }
+  for (let i = 0; i < 52 * 12; i++) {
+    const bubble = document.createElement('div');
+    bubble.classList.add('childhood', 'bubble');
+    childhoodContainer.append(bubble);
   }
 }
 
 function adolescenceBubbles() {
-  for (let i = 0; i < 52; i++) {
-    for (let j = 0; j < 7; j++) {
-      const bubble = document.createElement('div');
-      bubble.classList.add('adolescence', 'bubble');
-      adolescenceContainer.append(bubble);
-    }
+  for (let i = 0; i < 52 * 7; i++) {
+    const bubble = document.createElement('div');
+    bubble.classList.add('adolescence', 'bubble');
+    adolescenceContainer.append(bubble);
   }
 }
 
 function earlyAdulthoodBubbles() {
-  for (let i = 0; i < 52; i++) {
-    for (let j = 0; j < 15; j++) {
-      const bubble = document.createElement('div');
-      bubble.classList.add('early-adulthood', 'bubble');
-      earlyAdulthoodContainer.append(bubble);
-    }
+  for (let i = 0; i < 52 * 15; i++) {
+    const bubble = document.createElement('div');
+    bubble.classList.add('early-adulthood', 'bubble');
+    earlyAdulthoodContainer.append(bubble);
   }
 }
 
 function middleAdulthoodBubbles() {
-  for (let i = 0; i < 52; i++) {
-    for (let j = 0; j < 15; j++) {
-      const bubble = document.createElement('div');
-      bubble.classList.add('middle-adulthood', 'bubble');
-      middleAdulthoodContainer.append(bubble);
-    }
+  for (let i = 0; i < 52 * 15; i++) {
+    const bubble = document.createElement('div');
+    bubble.classList.add('middle-adulthood', 'bubble');
+    middleAdulthoodContainer.append(bubble);
   }
 }
 
 function matureAdulthoodBubbles() {
-  for (let i = 0; i < 52; i++) {
-    for (let j = 0; j < 30; j++) {
-      const bubble = document.createElement('div');
-      bubble.classList.add('mature-adulthood', 'bubble');
-      matureAdulthoodContainer.append(bubble);
-    }
+  for (let i = 0; i < 52 * 30; i++) {
+    const bubble = document.createElement('div');
+    bubble.classList.add('mature-adulthood', 'bubble');
+    matureAdulthoodContainer.append(bubble);
   }
 }
 
 function lateAdulthoodBubbles() {
-  for (let i = 0; i < 52; i++) {
-    for (let j = 0; j < 21; j++) {
-      const bubble = document.createElement('div');
-      bubble.classList.add('late-adulthood', 'bubble');
-      lateAdulthoodContainer.append(bubble);
-    }
+  for (let i = 0; i < 52 * 21; i++) {
+    const bubble = document.createElement('div');
+    bubble.classList.add('late-adulthood', 'bubble');
+    lateAdulthoodContainer.append(bubble);
   }
 }
 
