b94f5a155f754473a4043bc0be738d11320c6093
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-09-09 15:10:12 -0500

    Add bubbles- just blue for now

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const bubbleContainer = document.querySelector('#bubble-container');
+
+function createBubbles() {
+  bubbleContainer.style.gridTemplateRows = `repeat(52, 1fr)`;
+  for (let i = 0; i < 52; i++) {
+    for (let j = 0; j < 100; j++) {
+      const bubble = document.createElement('div');
+      bubble.classList.add('bubble');
+      bubbleContainer.append(bubble);
+    }
+  }
+}
+
+createBubbles();
