44ebad9c482c8a3d2e4346985cc7c75e1bfa26d0
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-09-09 16:13:54 -0500

    Add all bubbles, make last bubble black

--- script.js
+++ script.js
@@ -35,5 +35,49 @@ function adolescenceBubbles() {
   }
 }
 
+function earlyAdulthoodBubbles() {
+  for (let i = 0; i < 52; i++) {
+    for (let j = 0; j < 15; j++) {
+      const bubble = document.createElement('div');
+      bubble.classList.add('early-adulthood', 'bubble');
+      earlyAdulthoodContainer.append(bubble);
+    }
+  }
+}
+
+function middleAdulthoodBubbles() {
+  for (let i = 0; i < 52; i++) {
+    for (let j = 0; j < 15; j++) {
+      const bubble = document.createElement('div');
+      bubble.classList.add('middle-adulthood', 'bubble');
+      middleAdulthoodContainer.append(bubble);
+    }
+  }
+}
+
+function matureAdulthoodBubbles() {
+  for (let i = 0; i < 52; i++) {
+    for (let j = 0; j < 30; j++) {
+      const bubble = document.createElement('div');
+      bubble.classList.add('mature-adulthood', 'bubble');
+      matureAdulthoodContainer.append(bubble);
+    }
+  }
+}
+
+function lateAdulthoodBubbles() {
+  for (let i = 0; i < 52; i++) {
+    for (let j = 0; j < 21; j++) {
+      const bubble = document.createElement('div');
+      bubble.classList.add('late-adulthood', 'bubble');
+      lateAdulthoodContainer.append(bubble);
+    }
+  }
+}
+
 childhoodBubbles();
 adolescenceBubbles();
+earlyAdulthoodBubbles();
+middleAdulthoodBubbles();
+matureAdulthoodBubbles();
+lateAdulthoodBubbles();
