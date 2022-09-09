a7f1993f7674d748fc8aaa529e92f18c9ace61d1
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-09-09 15:29:21 -0500

    Add bubble containers for different life stages, add adolescence bubbles

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 'use strict';
 
-const bubbleContainer = document.querySelector('#bubble-container');
+const childhoodContainer = document.querySelector('#childhood-container');
+const adolescenceContainer = document.querySelector('#adolescence-container');
+const earlyAdulthoodContainer = document.querySelector(
+  '#early-adulthood-container'
+);
+const middleAdulthoodContainer = document.querySelector(
+  '#middle-adulthood-container'
+);
+const matureAdulthoodContainer = document.querySelector(
+  '#mature-adulthood-container'
+);
+const lateAdulthoodContainer = document.querySelector(
+  '#late-adulthood-container'
+);
 
-function createBubbles() {
-  bubbleContainer.style.gridTemplateRows = `repeat(52, 1fr)`;
+function childhoodBubbles() {
   for (let i = 0; i < 52; i++) {
-    for (let j = 0; j < 100; j++) {
+    for (let j = 0; j < 12; j++) {
       const bubble = document.createElement('div');
-      bubble.classList.add('bubble');
-      bubbleContainer.append(bubble);
+      bubble.classList.add('childhood-bubble');
+      childhoodContainer.append(bubble);
     }
   }
 }
 
-createBubbles();
+function adolescenceBubbles() {
+  for (let i = 0; i < 52; i++) {
+    for (let j = 0; j < 7; j++) {
+      const bubble = document.createElement('div');
+      bubble.classList.add('adolescence-bubble');
+      adolescenceContainer.append(bubble);
+    }
+  }
+}
+
+childhoodBubbles();
+adolescenceBubbles();
